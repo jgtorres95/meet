2fe95c0b35f3861b44bc672d8cf2a72e84feb054
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-03-05 16:53:02 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('117b18d251074bd096ee6a9ddae60f8d').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +24,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
